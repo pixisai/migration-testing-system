f9a601832db5257383fc60014316f5fb8026a39a
Denis <denis.zemtsov@gmail.com>
2022-12-02 17:20:23 +0300

    Added test models. Added test alembic versions. Implemented scratch of "up down" update.

--- migration_testing_system/__main__.py
+++ migration_testing_system/__main__.py
@@ -4,6 +4,37 @@ except ImportError:
     print("Alembic is not installed. Please install it with `pip install alembic`")
 
 
+from alembic import command
+
 # Тут надо будет создать объект Config, который будет содержать настройки для alembic
 # Предполагается что это будут параметризованные настройки, пользователь сможет сам настроить пути к папке миграций
 # Но можно установить дефолт на папку migrations
+from alembic.config import Config
+from alembic.script import ScriptDirectory
+
+config = Config("alembic.ini")
+# config = Config()
+config.set_main_option("script_location", "alembic")
+config.set_main_option(
+    "sqlalchemy.url", "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+)
+script = ScriptDirectory.from_config(config)
+
+
+def current_revision(self):
+    context.configure(connection=metadata.bind.connect(), transactional_ddl=True)
+    migration_context = self.context.get_context()
+    return migration_context.get_current_revision()
+
+
+print("current revision->")
+command.current(config)
+""" Get & sort migrations, from first to last"""
+revisions = list(script.walk_revisions("base", "heads"))
+revisions.reverse()
+print("---revisions---")
+for revision in revisions:
+    print(revision.revision)
+    """command.upgrade(config, revision.revision)
+    command.downgrade(config, revision.down_revision or '-1')
+    command.upgrade(config, revision.revision)"""
--- test/test_project/models.py
+++ test/test_project/models.py
@@ -11,3 +11,17 @@ class User(Base):
     id_ = Column("id", Integer, primary_key=True)
     first_name = Column(String(100), nullable=False)
     second_name = Column(String(100), nullable=False)
+    phone_id = Column("phone_id", Integer, nullable=True)
+
+
+class Phone(Base):
+    __tablename__ = "phone"
+    id_ = Column("id", Integer, primary_key=True)
+    pnone = Column(String(10), nullable=False)
+
+
+class Car(Base):
+    __tablename__ = "car"
+
+    id_ = Column("id", Integer, primary_key=True)
+    model = Column(String(100), nullable=False)
