feb92cf299c8a07c6ecf27c7c10a720843ce3ee0
Denis <denis.zemtsov@gmail.com>
2022-12-05 14:02:11 +0300

    Implemented iteration all "revisions".

--- migration_testing_system/__main__.py
+++ migration_testing_system/__main__.py
@@ -3,38 +3,34 @@ try:
 except ImportError:
     print("Alembic is not installed. Please install it with `pip install alembic`")
 
-
-from alembic import command
+from alembic.migration import MigrationContext
 
 # Тут надо будет создать объект Config, который будет содержать настройки для alembic
 # Предполагается что это будут параметризованные настройки, пользователь сможет сам настроить пути к папке миграций
 # Но можно установить дефолт на папку migrations
 from alembic.config import Config
 from alembic.script import ScriptDirectory
+from sqlalchemy import create_engine
 
 config = Config("alembic.ini")
-# config = Config()
-config.set_main_option("script_location", "alembic")
-config.set_main_option(
-    "sqlalchemy.url", "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
-)
+
+config.set_main_option("script_location", "/Users/denis/Documents/dev/pixisai/migration-testing-system/test/alembic")
+
 script = ScriptDirectory.from_config(config)
 
+engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
+conn = engine.connect()
+context = MigrationContext.configure(conn)
 
-def current_revision(self):
-    context.configure(connection=metadata.bind.connect(), transactional_ddl=True)
-    migration_context = self.context.get_context()
-    return migration_context.get_current_revision()
+current_rev = context.get_current_revision()
+print(f"current rev: {current_rev}")
 
+revisions = script.walk_revisions(current_rev, "heads")
 
-print("current revision->")
-command.current(config)
-""" Get & sort migrations, from first to last"""
-revisions = list(script.walk_revisions("base", "heads"))
-revisions.reverse()
 print("---revisions---")
 for revision in revisions:
-    print(revision.revision)
-    """command.upgrade(config, revision.revision)
-    command.downgrade(config, revision.down_revision or '-1')
-    command.upgrade(config, revision.revision)"""
+    if current_rev != revision.revision:
+        print(revision.revision)
+        """command.upgrade(config, revision.revision)
+        command.downgrade(config, revision.down_revision or '-1')
+        command.upgrade(config, revision.revision)"""
