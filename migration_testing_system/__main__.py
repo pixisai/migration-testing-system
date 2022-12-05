90ef6060b63528d3fdbf4aa45a6bd85eeb1e6384
dzemtsov-pixis <denis.zemtsov@pixis.ai>
2022-12-05 18:37:30 +0300

    Improved usage of application session/connection, instead of creating a new connection used the same. Implemented base accumulation logic for migration, first revision is current db revision next all script revision accumulated in each steps. (accumulating revision example: rev 1. rev 1,2. rev 1,2,3. rev 1,2,3,4...).

--- migration_testing_system/__main__.py
+++ migration_testing_system/__main__.py
@@ -3,34 +3,63 @@ try:
 except ImportError:
     print("Alembic is not installed. Please install it with `pip install alembic`")
 
-from alembic.migration import MigrationContext
-
-# Тут надо будет создать объект Config, который будет содержать настройки для alembic
+# Тут надо будет создать объект Config, который будет содержать настройки для alembic®
 # Предполагается что это будут параметризованные настройки, пользователь сможет сам настроить пути к папке миграций
 # Но можно установить дефолт на папку migrations
 from alembic.config import Config
+from alembic.migration import MigrationContext
 from alembic.script import ScriptDirectory
-from sqlalchemy import create_engine
+from sqlalchemy import engine_from_config, pool
+from test_project.models import metadata
 
+target_metadata = metadata
 config = Config("alembic.ini")
 
-config.set_main_option("script_location", "/Users/denis/Documents/dev/pixisai/migration-testing-system/test/alembic")
+config.set_main_option(
+    "script_location",
+    "/Users/denis/Documents/dev/pixisai/migration-testing-system/test/alembic",
+)
+config.set_main_option(
+    "sqlalchemy.url", "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+)
 
 script = ScriptDirectory.from_config(config)
 
-engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
-conn = engine.connect()
-context = MigrationContext.configure(conn)
 
-current_rev = context.get_current_revision()
-print(f"current rev: {current_rev}")
+def get_current_revision():
+    engine = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
-revisions = script.walk_revisions(current_rev, "heads")
+    with engine.connect() as connection:
+        context = MigrationContext.configure(connection)
+        current_rev = context.get_current_revision()
+        print(f"current db rev: {current_rev}")
+    return current_rev
 
-print("---revisions---")
-for revision in revisions:
-    if current_rev != revision.revision:
-        print(revision.revision)
-        """command.upgrade(config, revision.revision)
+
+def step(revision):
+    print(f"step->:{revision}")
+    """ command.upgrade(config, revision.revision)
         command.downgrade(config, revision.down_revision or '-1')
         command.upgrade(config, revision.revision)"""
+
+
+def walk_revisions(from_revision):
+    revisions = list(script.walk_revisions(from_revision, "heads"))
+    revisions.reverse()
+
+    rev_accum = list()
+
+    for revision in revisions:
+        if from_revision == revision.revision:
+            continue
+        print(f"for script revision: {revision.revision}")
+        rev_accum.append(revision.revision)
+        for rev in rev_accum:
+            step(rev)
+
+
+walk_revisions(get_current_revision())
